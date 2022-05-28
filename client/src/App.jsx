2e6031a23927c263418d85830605401fc61fd4f7
nattawat.wib@gmail.com <nattawat.wib@gmail.com>
2022-05-28 22:47:22 +0700

    setup vite

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Container, Button } from '@mui/material'
+import './App.scss'
+
+function App() {
+  return (
+    <Container>
+      <h1 className='text-4xl'> test </h1>
+      <Button variant='contained' > test </Button>
+    </Container>
+  )
+}
+
+export default App
