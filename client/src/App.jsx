4434d489b8814517e629de5673bb25427ac7f589
nattawat.wib@gmail.com <nattawat.wib@gmail.com>
2022-05-28 23:16:12 +0700

    set mui theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.scss'
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import theme from './style/theme';
 
 import Login from './pages/login';
 
 function App() {
     return (
-        <>
-            <Login />
-        </>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <Login />
+            </ThemeProvider>
+        </StyledEngineProvider>
     )
 }
 export default App
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,24 @@
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+
+import styled from 'styled-components';
+
 const Login = () => {
     return (
-        <h1> Login Page </h1>
+        <LoginPageWrapper>
+            <Paper>
+                <h1> Login Page </h1>
+            </Paper>
+        </LoginPageWrapper>
     )
 }
 
 export default Login;
+
+const LoginPageWrapper = styled(Container)`
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: red;
+`   
