d9ac697af71f3efa2a1e1bb6ed6093bf0b33cc7a
nattawat-wib <nattawat.wib@gmail.com>
2022-06-09 17:19:17 +0700

    heroku

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.use("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "index.html"));
+        res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
 }
 
